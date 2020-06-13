a8b037fcb620b0813bb57d1d26bf8446a1b13ffa
emir <contact@ekherchi.fr>
2020-06-13 10:59:28 +0200

    Utilisation du local storage pour stocker les infos lors du fetch API

--- frontend/teddy_sale.js
+++ frontend/teddy_sale.js
@@ -1,3 +1,5 @@
+
+
 /****** Variables elements du dom******/
 const nomDeLOurson = document.getElementById("title");
 const imgOurson = document.getElementById("image");
@@ -17,113 +19,129 @@ const garf = document.getElementById("garfunkel");
 
 /******Fetch et Promise******/
 
-fetch("http://localhost:3000/api/teddies/").then(function (response) {
-  response
-    .json()
-    .then(function (product) {
-      class teddyBear {
-        constructor(name, price, description, colors, id, image) {
-          this.name = name;
-          this.price = price;
-          this.description = description;
-          this.colors = colors;
-          this.id = id;
-          this.image = image;
-        }
-        teddys() {
-          nomDeLOurson.innerHTML = this.name;
-          imgOurson.src = this.image;
-          imgOurson.style.display = "block";
-          descriptionOurson.innerHTML = this.description;
-          id.innerHTML = this.id;
-          id.style.display = "none";
-          price.innerHTML = (this.price / 100).toFixed(2) + " €";
-          formChoixCouleurs.style.display = "block";
-          divBtnAddToCart.style.display = "block";
-
-          
-          dropdownMenuCouleurs.options.length = 0; 
-          let defaultOption = document.createElement("option"); 
-          defaultOption.text = "Personalisez votre ourson";
-          dropdownMenuCouleurs.add(defaultOption); 
-          for (let i = 0; i < this.colors.length; i++) {
-            let choice;
-            choice = document.createElement("option"); 
-            dropdownMenuCouleurs.add(choice); 
-            choice.innerHTML = this.colors[i]; 
-          }
-        }
-      }
-
-    /****** Création des instances******/
-
-      const norbert = new teddyBear(
-        product[0].name,
-        product[0].price,
-        product[0].description,
-        product[0].colors,
-        product[0]._id,
-        product[0].imageUrl
-      );
-      const arnold = new teddyBear(
-        product[1].name,
-        product[1].price,
-        product[1].description,
-        product[1].colors,
-        product[1]._id,
-        product[1].imageUrl
-      );
-      const lennyAndCarl = new teddyBear(
-        product[2].name,
-        product[2].price,
-        product[2].description,
-        product[2].colors,
-        product[2]._id,
-        product[2].imageUrl
-      );
-      const gustav = new teddyBear(
-        product[3].name,
-        product[3].price,
-        product[3].description,
-        product[3].colors,
-        product[3]._id,
-        product[3].imageUrl
-      );
-      const garfunkel = new teddyBear(
-        product[4].name,
-        product[4].price,
-        product[4].description,
-        product[4].colors,
-        product[4]._id,
-        product[4].imageUrl
-      );
-
-      // création du rendu dynamique sur la page au click
-
-      const elementDeNavigation = [norb, arn, lennyNCarl, gus, garf];
-      const ours = [norbert, arnold, lennyAndCarl, gustav, garfunkel];
-
-      for (let i = 0; i < elementDeNavigation.length; i++) {
-        elementDeNavigation[i].addEventListener("click", function () {
-          ours[i].teddys();
-        });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+fetch("http://localhost:3000/api/teddies/")
+.then(function (response) {
+  if (response.status !== 200) {
+    console.log("erreur: " + response.status);
+  }
+  response.json()
+  .then(function (data) {
+    localStorage.setItem("products", JSON.stringify(data));
+  });
+})
+.catch(function (error) {
+  console.log(error + " erreur fetch");
 });
 
-/****** Création du PANIER dans le Local Storage******/
+
+/******Création de l'objet Teddy Bear******/
+
+const product = JSON.parse(localStorage.getItem("products"));
+
+  class teddyBear {
+    constructor(name, price, description, colors, id, image) {
+      this.name = name;
+      this.price = price;
+      this.description = description;
+      this.colors = colors;
+      this.id = id;
+      this.image = image;
+    }
+    teddys() {
+      nomDeLOurson.innerHTML = this.name;
+      imgOurson.src = this.image;
+      imgOurson.style.display = "block";
+      descriptionOurson.innerHTML = this.description;
+      id.innerHTML = this.id;
+      id.style.display = "none";
+      price.innerHTML = (this.price / 100).toFixed(2) + " €";
+      formChoixCouleurs.style.display = "block";
+      divBtnAddToCart.style.display = "block";
+
+      dropdownMenuCouleurs.options.length = 0;
+      let defaultOption = document.createElement("option");
+      defaultOption.text = "Personalisez votre ourson";
+      dropdownMenuCouleurs.add(defaultOption);
+      for (let i = 0; i < this.colors.length; i++) {
+        let choice;
+        choice = document.createElement("option");
+        dropdownMenuCouleurs.add(choice);
+        choice.innerHTML = this.colors[i];
+      }
+    }
+  }
+
+  /****** Création des instances******/
+
+  const norbert = new teddyBear(
+    product[0].name,
+    product[0].price,
+    product[0].description,
+    product[0].colors,
+    product[0]._id,
+    product[0].imageUrl
+  );
+  const arnold = new teddyBear(
+    product[1].name,
+    product[1].price,
+    product[1].description,
+    product[1].colors,
+    product[1]._id,
+    product[1].imageUrl
+  );
+  const lennyAndCarl = new teddyBear(
+    product[2].name,
+    product[2].price,
+    product[2].description,
+    product[2].colors,
+    product[2]._id,
+    product[2].imageUrl
+  );
+  const gustav = new teddyBear(
+    product[3].name,
+    product[3].price,
+    product[3].description,
+    product[3].colors,
+    product[3]._id,
+    product[3].imageUrl
+  );
+  const garfunkel = new teddyBear(
+    product[4].name,
+    product[4].price,
+    product[4].description,
+    product[4].colors,
+    product[4]._id,
+    product[4].imageUrl
+  );
+  // création du rendu dynamique sur la page au click
+
+  const elementDeNavigation = [norb, arn, lennyNCarl, gus, garf];
+  const ours = [norbert, arnold, lennyAndCarl, gustav, garfunkel];
+
+  for (let i = 0; i < elementDeNavigation.length; i++) {
+    elementDeNavigation[i].addEventListener("click", function () {
+      ours[i].teddys();
+    });
+  }
+
+
+
  
 
-if (localStorage.getItem("cart") === null ) {
-  let panierInit = []; 
+
+
+
+
+
+
+/****** Création du PANIER dans le Local Storage******/
+
+if (localStorage.getItem("cart") === null) {
+  let panierInit = [];
   localStorage.setItem("cart", JSON.stringify(panierInit));
 }
-let cart = JSON.parse(localStorage.getItem("cart")); 
-numberEltCart.innerHTML = "(" + cart.length + ")"; 
-
+let cart = JSON.parse(localStorage.getItem("cart"));
+numberEltCart.innerHTML = "(" + cart.length + ")";
 
 /****** Fonction Pour ajout au panier******/
 btnAddToCart.addEventListener("click", function () {
@@ -138,6 +156,6 @@ btnAddToCart.addEventListener("click", function () {
     ]);
 
     localStorage.setItem("cart", JSON.stringify(cart)); //Ajout au local storage depuis le array local
-    numberEltCart.innerHTML = "(" + cart.length + ")"; 
+    numberEltCart.innerHTML = "(" + cart.length + ")";
   }
 });
