4bf4d87a19b8e5c4c125ee19e0bbe48ff290d388
emir <contact@ekherchi.fr>
2020-06-23 12:11:40 +0200

    architecture cart.js

--- frontend/Js/cart.js
+++ frontend/Js/cart.js
@@ -1,34 +1,29 @@
-/****** Variables du DOM******/
-
-const numberEltCart = document.getElementById("number");
-const table = document.getElementById("tableau");
-
-const mainHtml = document.querySelector(".cart");
-const loadingSpinner = document.querySelector(".lds-spinner");
+/**********/
+/****** Tableau dynamique******/
+/**********/
 
 /****** Récupération des elements dans le local Storage******/
-
+const numberEltCart = document.getElementById("number");
 const cart = JSON.parse(localStorage.getItem("cart"));
+const mainElementPage = document.getElementById("mainBalise");
 
 if (cart === null || cart.length < 1) {
   numberEltCart.innerHTML = "(0)";
-  mainHtml.style.display = "none";
+  mainElementPage.style.display = "none";
   const cartEmpty = document.createElement("div");
-  cartEmpty.innerHTML =
-    "<h1>Oups...</h1><br><h3>Votre panier est vide !<br>Retrouvez nos différents produits sur notre page d'accueil.</h3>";
-  cartEmpty.style.marginTop = "250px";
-  cartEmpty.style.lineHeight = "80px";
-  cartEmpty.style.textAlign = "center";
+  cartEmpty.classList.add("empty");
+  cartEmpty.innerHTML = "<h1>Oups...</h1><br><h3>Votre panier est vide !<br>Retrouvez nos différents produits sur notre page d'accueil.</h3>";
   document.body.appendChild(cartEmpty);
 } else {
   numberEltCart.innerHTML = "(" + cart.length + ")";
 
   /****** Création du tableau HTML pour chaque produits du panier******/
 
+  const table = document.getElementById("tableau");
   for (let i = 0; i < cart.length; i++) {
     let newLigne = document.createElement("tr");
     table.appendChild(newLigne);
-    newLigne.className += "tableLine";
+    newLigne.classList.add("tableLine");
 
     for (let j = 0; j < cart[i].length; j++) {
       let newCol = document.createElement("th");
@@ -37,35 +32,36 @@ if (cart === null || cart.length < 1) {
     }
   }
 
-  /****** Ajout bouton supprimer et du prix total******/
+  /****** Ajout :  bouton supprimer / prix total ******/
 
   const line = document.getElementsByClassName("tableLine");
 
   let total = 0;
   const totalPrice = [];
-  const totalCart = document.getElementById("prixTotal");
+  const totalCartSpan = document.getElementById("prixTotal");
 
   for (i = 0; i < line.length; i++) {
+    //suppression des ID produits
     const allID = line[i].cells[3];
     allID.style.display = "none";
 
+    //Création BTN supprimer
     const btn = document.createElement("button");
-    btn.className += "btnLine";
+    btn.classList.add("btnLine");
     line[i].appendChild(btn);
     btn.innerHTML = "X";
 
-    const allPrice = line[i].cells[2].textContent;
-    totalPrice.push(parseFloat(allPrice));
+    //récupération des prix
+    const allPriceCells = line[i].cells[2].textContent;
+    totalPrice.push(parseFloat(allPriceCells));
   }
 
   for (let i = 0; i < totalPrice.length; i++) {
     total += totalPrice[i];
   }
-  totalCart.innerHTML =
-    total + " €" + "<br>Nombre d'articles dans le panier: " + cart.length;
+  totalCartSpan.innerHTML = total + " €" + "<br>Nombre d'articles dans le panier: " + cart.length;
 
   const removeBtn = document.getElementsByClassName("btnLine");
-
   for (let i = 0; i < removeBtn.length; i++) {
     removeBtn[i].addEventListener("click", function () {
       cart.splice(i, 1); //supprime la ligne correspondante à l'index du btn
@@ -74,6 +70,11 @@ if (cart === null || cart.length < 1) {
     });
   }
 }
+
+
+/**********/
+/****** Formulaire de validation de commande******/
+/**********/
 
 /****** REGEX pour validation du panier******/
 
@@ -84,6 +85,7 @@ const ville = document.getElementById("city");
 const mail = document.getElementById("email");
 
 const btnSubmitForm = document.getElementById("envoiDuPanier");
+const loadingSpinner = document.querySelector(".lds-spinner");
 
 const regexLettersOnly = /^[a-zA-Zàâéêèìîôùûçëäöüï-]+$/;
 const regexEmail = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/;
@@ -91,23 +93,17 @@ const regexCityAndAdress = /^[a-zA-Z0-9àâéêèìîôùûçëäöüï' _-]+$/;
 
 /****** Initialisation de l'Objet qui sera envoyé******/
 
-let obj;
-let products;
-
-const getObjetID = () => {
-  products = [];
+let objectToSend ;
+let contact;
+let productsId;
+const getAllProductsID = () => {
+  productsId = [];
   for (let i = 0; i < cart.length; i++) {
-    products.push(cart[i][3]);
+    productsId.push(cart[i][3]);
   }
 };
 
-let contact = {
-  firstName: "",
-  lastName: "",
-  address: "",
-  city: "",
-  email: "",
-};
+
 
 /****** fonction vérification formulaire******/
 
@@ -116,99 +112,93 @@ const modalText = document.getElementById("modaltext");
 const closeModal = document.getElementById("closemodal");
 
 const modalAlert = (text) => {
-  mainHtml.style.opacity = "0.2";
-  modal.style.visibility = "visible";
-  modalText.innerHTML = text;
+  mainElementPage.style.opacity = "0.2";
+  modal.style.display = "block";
+  modalText.innerHTML = "Erreur dans le format de votre " + text;
   closeModal.addEventListener("click", function () {
-    modal.style.visibility = "hidden";
-    mainHtml.style.opacity = "1";
+    modal.style.display = "none";
+    mainElementPage.style.opacity = "1";
   });
 };
 
 const checkFormInput = () => {
   if (regexLettersOnly.test(fName.value) == false) {
-    fName.style.backgroundColor = "#ffc0cb";
-    modalAlert("Erreur dans le format de votre prénom");
+    fName.className = "form-control alert alert-danger";
+    modalAlert("prénom");
   } else {
-    contact.firstName = fName.value;
-    fName.style.backgroundColor = "#bcf5bc";
+    fName.className = "form-control alert alert-success";
     if (regexLettersOnly.test(lName.value) == false) {
-      lName.style.backgroundColor = "#ffc0cb";
-      modalAlert("Erreur dans le format de votre nom");
+      lName.className = "form-control alert alert-danger";
+      modalAlert("nom");
     } else {
-      lName.style.backgroundColor = "#bcf5bc";
-      contact.lastName = lName.value;
+      lName.className = "form-control alert alert-success";
       if (regexCityAndAdress.test(adresse.value) == false) {
-        adresse.style.backgroundColor = "#ffc0cb";
-        modalAlert("Erreur dans le format de votre adresse");
+        adresse.className = "form-control alert alert-danger";
+        modalAlert("adresse");
       } else {
-        adresse.style.backgroundColor = "#bcf5bc";
-        contact.address = adresse.value;
+        adresse.className = "form-control alert alert-success";
         if (regexCityAndAdress.test(ville.value) == false) {
-          ville.style.backgroundColor = "#ffc0cb";
-          modalAlert("Erreur dans le format de votre ville");
+          ville.className = "form-control alert alert-danger";
+          modalAlert("ville");
         } else {
-          ville.style.backgroundColor = "#bcf5bc";
-          contact.city = ville.value;
+          ville.className = "form-control alert alert-success";
           if (regexEmail.test(mail.value) == false) {
-            mail.style.backgroundColor = "#ffc0cb";
-            modalAlert("Erreur dans le format de votre adresse e-mail");
+            mail.className = "form-control alert alert-danger";
+            modalAlert("adresse e-mail");
           } else {
-            mail.style.backgroundColor = "#bcf5bc";
-            contact.email = mail.value;
-
-            // quand tous les champs du formulaire sont validés-->
+            mail.className = "form-control alert alert-success";    
             return true;
-          }
-        }
-      }
-    }
-  }
+          };
+        };
+      };
+    };
+  };
 };
 
+/* Fonction envoi des données */
+
 const sendRequest = () => {
-  getObjetID();
+  
+  //création de l'objet
+  contact = {
+    firstName: fName.value,
+    lastName: lName.value,
+    address: adresse.value,
+    city: ville.value,
+    email: mail.value,
+  };
+  getAllProductsID();
+  objectToSend = { contact, products : productsId }; 
+  const ObjectSend = JSON.stringify(objectToSend);
 
-  obj = { contact, products }; // ajout de contact et produits à l'objet obj.
-  const toSend = JSON.stringify(obj);
 
-  // envoi des données au serveur
+  // envoi de l'objet et traitement de la réponse
   const request = new XMLHttpRequest();
-
   request.onload = function () {
     if (this.readyState == XMLHttpRequest.DONE) {
       response = JSON.parse(this.responseText);
       let responseOrder;
       responseOrder = response.orderId;
 
-      mainHtml.style.display = "none";
+      mainElementPage.style.display = "none";
       const thanksCustomer = document.createElement("div");
-      thanksCustomer.innerHTML =
-        "Merci pour votre commande " +
-        contact.firstName +
-        "<br>Commande numéro : " +
-        responseOrder +
-        "<br>Vous allez être redirigé vers la page d'accueil d'ici quelques secondes";
-
-      thanksCustomer.style.marginTop = "30%";
-      thanksCustomer.style.fontSize = "20px";
-      thanksCustomer.style.textAlign = "center";
+      thanksCustomer.classList.add("thanks");
+      thanksCustomer.innerHTML = "Merci pour votre commande " + contact.firstName + "<br>Commande numéro : " + responseOrder + "<br>Vous allez être redirigé vers la page d'accueil d'ici quelques secondes";
       document.body.insertBefore(thanksCustomer, loadingSpinner);
-      loadingSpinner.style.display = "inline-block";
+      loadingSpinner.style.display = "block";
     }
   };
-
   request.open("POST", "http://localhost:3000/api/teddies/order");
   request.setRequestHeader("Content-Type", "application/json");
-  request.send(toSend);
+  request.send(ObjectSend);
 
   localStorage.removeItem("cart");
-
   setTimeout(() => {
     window.location.href = "index.html";
   }, 5000);
 };
 
+//Evenement sur le bouton validation du formulaire
 btnSubmitForm.addEventListener("click", function (e) {
   e.preventDefault();
   if (checkFormInput()) {
